e76d62e5266d9e485bcdea916a0104d41c145e05
elsessor <lagad@gbox.adnu.edu.ph>
2025-10-11 00:00:00 +0800

    TASK - 01 = TaskModel

---
+++ backend/src/models/Task.js
@@ -0,0 +1,10 @@
+class Task {
+  constructor(id, title, description, createdBy) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.createdBy = createdBy;
+  }
+}
+
+module.exports = Task;
